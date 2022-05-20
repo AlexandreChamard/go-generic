5f07a34f1002c1dd2467a843eb41f3a44e6a897c
alexandre <alexandre.chamard-bois@epitech.eu>
2022-05-20 18:08:50 +0200

    ✨ add Ptr Getter for queue and stack

--- queue/queue.go
+++ queue/queue.go
@@ -16,6 +16,8 @@ type queue[T any] []T
 func (this queue[T]) Empty() bool  { return this.Size() == 0 }
 func (this queue[T]) Size() int    { return len(this) }
 func (this queue[T]) Front() T     { return this[0] }
+func (this queue[T]) FrontPtr() *T { return &this[0] }
 func (this queue[T]) Back() T      { return this[len(this)-1] }
+func (this queue[T]) BackPtr() *T  { return &this[len(this)-1] }
 func (this *queue[T]) Push(info T) { *this = append(*this, info) }
 func (this *queue[T]) Pop()        { *this = (*this)[1:] }
--- stack/stack.go
+++ stack/stack.go
@@ -15,5 +15,6 @@ type stack[T any] []T
 func (this stack[T]) Empty() bool  { return this.Size() == 0 }
 func (this stack[T]) Size() int    { return len(this) }
 func (this stack[T]) Top() T       { return this[len(this)-1] }
+func (this stack[T]) TopPtr() *T   { return &this[len(this)-1] }
 func (this *stack[T]) Push(info T) { *this = append(*this, info) }
 func (this *stack[T]) Pop()        { *this = (*this)[:len(*this)-1] }
